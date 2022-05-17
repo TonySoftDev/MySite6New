af1d5135ec27becdb1c8f8d8ee31f74ae516614f
Antonio Peruzzi <tony.eps@hotmail.it>
2022-05-17 08:23:39 +0200

    Gallery Test 1.2, LoadinvAwesome senza sfondo

---
+++ MySite6New/Shared/PicsumItem.cs
@@ -0,0 +1,12 @@
+﻿namespace MySite6New.Shared
+{
+    public class PicsumItem
+    {
+        public int Id { get; set; }
+        public string Author { get; set; } = string.Empty;
+        public int Width { get; set; }
+        public int Height { get; set; }
+        public string Url { get; set; } = string.Empty;
+        public string Download_url { get; set; } = string.Empty;
+    }
+}
