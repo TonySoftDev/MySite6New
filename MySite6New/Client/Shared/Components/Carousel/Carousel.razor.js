b17fcddfa6aae9c39f4b3a0d9e60eae98e42128c
Antonio Peruzzi <tony.eps@hotmail.it>
2022-06-18 11:56:21 +0200

    Fix Carousel: js isolation

---
+++ MySite6New/Client/Shared/Components/Carousel/Carousel.razor.js
@@ -0,0 +1,6 @@
+﻿
+export function startCarousel(carouselId) {
+    var myCarousel = document.getElementById(carouselId);
+    var carousel = new bootstrap.Carousel(myCarousel);
+    carousel.cycle(); // la chiamata a cycle fa partire subito il carousel
+}
